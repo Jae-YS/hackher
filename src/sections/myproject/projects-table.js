fb1e548328379db923fcfdfd648f0d495e6c2608
Jae Young Seo <jaeyoungseo@vl965-172-31-28-226.wireless.umass.edu>
2024-02-17 16:18:11 -0500

    my projects

---
+++ src/sections/myproject/projects-table.js
@@ -0,0 +1,123 @@
+import PropTypes from "prop-types";
+import {
+  Avatar,
+  Box,
+  Card,
+  Checkbox,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Scrollbar } from "src/components/scrollbar";
+import { getInitials } from "src/utils/get-initials";
+
+export const ProjectsTable = (props) => {
+  const {
+    count = 0,
+    items = [],
+    onDeselectAll,
+    onDeselectOne,
+    onPageChange = () => {},
+    onRowsPerPageChange,
+    onSelectAll,
+    onSelectOne,
+    page = 0,
+    rowsPerPage = 0,
+    selected = [],
+  } = props;
+
+  const selectedSome = selected.length > 0 && selected.length < items.length;
+  const selectedAll = items.length > 0 && selected.length === items.length;
+
+  return (
+    <Card>
+      <Scrollbar>
+        <Box sx={{ minWidth: 800 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {/* <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedAll}
+                    indeterminate={selectedSome}
+                    onChange={(event) => {
+                      if (event.target.checked) {
+                        onSelectAll?.();
+                      } else {
+                        onDeselectAll?.();
+                      }
+                    }}
+                  />
+                </TableCell> */}
+                <TableCell>Project Name</TableCell>
+                <TableCell>Tech Stack Used</TableCell>
+                <TableCell>Date Created</TableCell>
+                <TableCell>Last Modified</TableCell>
+                <TableCell>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((project) => {
+                const isSelected = selected.includes(project.id);
+
+                return (
+                  <TableRow hover key={project.id} selected={isSelected}>
+                    <TableCell>
+                      <Stack alignItems="center" direction="row" spacing={2}>
+                        <Typography variant="subtitle2">{project.nameOfProject}</Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell>{project.techStackUsed}</TableCell>
+                    <TableCell>{project.dateCreated}</TableCell>
+                    <TableCell>{project.dateLastModified}</TableCell>
+                    <TableCell
+                      style={{
+                        fontWeight: "bold",
+                        color:
+                          project.status === "Done"
+                            ? "green"
+                            : project.status === "In Progress"
+                            ? "gray"
+                            : "red", // Color for the third status option
+                      }}
+                    >
+                      {project.status}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+      </Scrollbar>
+      <TablePagination
+        component="div"
+        count={count}
+        onPageChange={onPageChange}
+        onRowsPerPageChange={onRowsPerPageChange}
+        page={page}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+};
+
+ProjectsTable.propTypes = {
+  count: PropTypes.number,
+  items: PropTypes.array,
+  onDeselectAll: PropTypes.func,
+  onDeselectOne: PropTypes.func,
+  onPageChange: PropTypes.func,
+  onRowsPerPageChange: PropTypes.func,
+  onSelectAll: PropTypes.func,
+  onSelectOne: PropTypes.func,
+  page: PropTypes.number,
+  rowsPerPage: PropTypes.number,
+  selected: PropTypes.array,
+};
