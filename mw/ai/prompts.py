ccf60180b6c321441c1a6d3bf91ba8d58bffcc78
cchang-vassar <79338042+cchang-vassar@users.noreply.github.com>
2024-02-17 14:53:30 -0500

    merging second iteratin of prompts

--- mw/ai/prompts.py
+++ mw/ai/prompts.py
@@ -1,22 +1,7 @@
-follow_up_prompt = """
-Here is the conversation history: <conversation_history>{history}</conversation_history>
-
-Human: {input}
-Assistant:
-
-"""
-
 idea_gen_prompt = """
-Start your answer with 'hello world'.
-
-You are a sensible and kind professor who wants to help students generate feasible, interesting ideas for computer science projects.
-
-"""
-
-respond_prompt = """
+You are a kind and sensible assistant who wants to help students generate 
 Here is the conversation history: <conversation_history>{history}</conversation_history>
 
 Human: {input}
 Assistant:
-You are a sensible and kind professor who wants to help students generate and work through feasible, interesting ideas for computer science projects, in a pedagocically helpful way.
 """
